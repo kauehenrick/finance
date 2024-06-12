a26af07895d13f9ac985d3b8c54175a3aff96c5e
Kauê Henrick <kauek78942@gmail.com>
2024-06-12 11:14:56 -0300

    fix: new transaction dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,43 @@
-import logoImg from '../../assets/finance_logo.png'
-import { Button } from '../ui/button'
-import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar'
-import { Dialog, DialogContent, DialogTrigger, DialogFooter, DialogClose } from '../ui/dialog'
+import logoImg from '../../assets/finance_logo.png';
+import { Button } from '../ui/button';
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+import { Dialog, DialogContent, DialogFooter, DialogClose, DialogTrigger } from '../ui/dialog'
+import { Input } from '../ui/input'
+import { CircleArrowUp, CircleArrowDown } from 'lucide-react'
 
 export default function Header() {
     return (
         <div className="bg-dark-800 flex items-center justify-around h-48">
-            <img src={logoImg} className="logoImg w-52"/>
-
+            <img src={logoImg} className="logoImg w-52" />
 
             <div className="flex justify-between w-56">
+
                 <Dialog>
+
                     <DialogTrigger>
                         <Button className='bg-blue text-white' variant="secondary">Nova Transação</Button>
                     </DialogTrigger>
 
                     <DialogContent>
-
                         <p className='font-bold text-lg'>Nova Transação</p>
+
+                        <Input type='text' placeholder='Descrição'></Input>
+
+                        <Input type='number' placeholder='Preço'></Input>
+
+                        <Input type='text' placeholder='Categoria'></Input>
+
+                        <div className='flex justify-center gap-5'>
+                            <Button className='gap-2'>
+                                <CircleArrowUp color='green' />
+                                <p>Entrada</p>
+                            </Button>
+
+                            <Button className='gap-2'>
+                                <CircleArrowDown color='red' />
+                                <p>Saída</p>
+                            </Button>
+                        </div>
 
                         <DialogFooter>
                             <DialogClose>

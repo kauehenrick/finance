bc290954043eab26eec7d552dd963f0e1db4c1ae
Kauê Henrick <kauehenrick@proton.me>
2024-07-02 16:56:06 -0300

    blocked

--- src/components/DisableButton/index.tsx
+++ src/components/DisableButton/index.tsx
@@ -1,9 +1,6 @@
 import { Trash2 } from "lucide-react";
-import { useTransactionStore } from "@/stores/TransactionStore";
 
 export default function DisableButton() {
-    let store = useTransactionStore();
-    let {deleteTransaction} = store;
 
     return (
         <div className="bg-red p-2 rounded-lg hover:brightness-75 cursor-pointer"><Trash2 color="background" size={16} /></div>

86c6528861decbc74510768e32885459c9fb22ee
Kauê Henrick <kauehenrick@proton.me>
2024-08-23 17:46:44 -0300

    first commit

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -2,6 +2,13 @@ import { useEffect } from "react";
 import { TransactionProps, useTransactionStore } from "@/stores/TransactionStore";
 import { DataTable } from "./data-table"
 import { columns } from "./columns"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 export default function TransactionsTable() {
     let store = useTransactionStore();
@@ -22,7 +29,20 @@ export default function TransactionsTable() {
 
     return (
         <div className="bg-white border rounded-lg p-2 w-10/12 m-auto mt-20">
-            { isLoading && <div className="p-3">Carregando...</div> }
+            <div className="mt-3 mb-5">
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Selecione a conta" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+
+            {isLoading && <div className="p-3">Carregando...</div>}
 
             <DataTable columns={columns} data={transactionsActive} />
         </div>

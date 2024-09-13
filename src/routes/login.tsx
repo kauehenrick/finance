eec06ab336d7aa5d91b695d9c57bc7dadfcaac60
Kauê Henrick <kauehenrick@proton.me>
2024-09-13 11:37:17 -0300

    refactor: fixed function call

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -86,7 +86,7 @@ export default function Login() {
                     email: user.email,
                     name: user.displayName,
                 });
-                onLogin();
+                handleLogin(user)
             }
         })
     }
